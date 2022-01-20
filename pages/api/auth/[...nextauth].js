1331d1800d934e66f024b39a45d047c12154d943
jrjradler <adlerrjake@gmail.com>
2022-01-19 20:15:05 -0500

    1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,25 +2,25 @@ import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
 
 export default NextAuth({
-  providers: [
-    SpotifyProvider({
-      authorization:
-        'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private',
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    }),
-  ],
+    providers: [
+        SpotifyProvider({
+            authorization: 'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private',
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+        }),
+    ],
 
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.refresh_token;
-      }
-      return token;
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.refresh_token;
+            }
+            return token;
+        },
+        async session(session, user) {
+            session.user = user;
+            return session;
+        },
     },
-    async session(session, user) {
-      session.user = user;
-      return session;
-    },
-  },
+    debug: true
 });
